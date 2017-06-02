33a9459db0deb378543dc204be4f54e2ae016d3b
guanyabin <guanyabin2010@163.com>
2017-06-02 10:39:19 +0800

    关联的弹出窗体程序文件

---
+++ InfoFrm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test0311
+{
+    public partial class InfoFrm : Form
+    {
+        public InfoFrm(string strInfo)
+        {
+            InitializeComponent();
+            rtxtInfo.Text = strInfo;
+        }
+
+        private void InfoFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
